eec7cd213e55c17d1edc3f1384717b13eb3c3e23
cnaziru <cnaziru@cisco.com>
2019-08-05 12:15:14 +0300

    Updates

---
+++ precache-manifest.264edfc5da01e9878159e4903639c39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9394c50e45ee01664a4cfa5617b67de9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43c7f33918f338984b6d",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "0ee7bb801025c61a489e",
+    "url": "/static/css/main.eef33757.chunk.css"
+  },
+  {
+    "revision": "43c7f33918f338984b6d",
+    "url": "/static/js/2.cfaae486.chunk.js"
+  },
+  {
+    "revision": "0ee7bb801025c61a489e",
+    "url": "/static/js/main.e4cffc57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
